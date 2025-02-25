9013bc865e67c957305c15b3d58a904011d19007
Cole Coleman <colecoleman@me.com>
2025-02-25 00:25:47 -0600

    site attempt

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Add this line for relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
